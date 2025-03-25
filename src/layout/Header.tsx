8e42fa61d1ac39f3b38bc56f6dc7310f9db3e74a
Heuluck Lu <sonicth@foxmail.com>
2025-03-25 16:22:37 +0800

    chore: replace llpkgstore with LLPkgStore

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div className="sticky top-0 flex flex-row items-center bg-white/80 px-8 py-2 shadow-sm backdrop-blur-lg">
             <div className="mr-auto flex flex-col text-left">
-                <span className="text-3xl font-bold">llpkgstore</span>
+                <span className="text-3xl font-bold">LLPkgStore</span>
                 <span className="text-sm text-gray-600">
                     Version map searching for llpkg.
                 </span>
