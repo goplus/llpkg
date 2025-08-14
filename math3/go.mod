63691ecdf894365b84e64455397209231b3342bc
谢秀煌 <xiexiuhuang@xiexiuhuangdeMacBook-Pro.local>
2025-08-14 15:55:31 +0800

    11

--- math3/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/goplus/llpkg/math
-
-go 1.23
-
-require github.com/goplus/lib v0.2.0
--- math3/math_autogen_link.go
+++
@@ -1,5 +0,0 @@
-package math
-
-import _ "github.com/goplus/lib/py"
-
-const LLGoPackage string = "py.math"
