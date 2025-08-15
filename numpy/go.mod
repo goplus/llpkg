7f7bee18456f64b73092c7a993dd7dc989340452
谢秀煌 <xiexiuhuang@xiexiuhuangdeMacBook-Pro.local>
2025-08-15 11:01:17 +0800

    del

--- numpy/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/goplus/llpkg/numpy
-
-go 1.23
-
-require github.com/goplus/lib v0.2.0
--- numpy/numpy_autogen_link.go
+++
@@ -1,5 +0,0 @@
-package numpy
-
-import _ "github.com/goplus/lib/py"
-
-// LLGoPackage is defined in the main binding file
